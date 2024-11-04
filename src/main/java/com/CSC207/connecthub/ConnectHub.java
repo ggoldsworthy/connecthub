2391489b40b6ec2f519416aaf5a48be2f6e0e10b
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-04 13:45:22 -0500

    more initialization

---
+++ src/main/java/com/CSC207/connecthub/ConnectHub.java
@@ -0,0 +1,13 @@
+package com.CSC207.connecthub;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class ConnectHub {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(ConnectHub.class, args);
+	}
+
+}
