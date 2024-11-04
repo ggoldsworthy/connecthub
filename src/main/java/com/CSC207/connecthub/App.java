5e2daf8caa2f62664a24059e9fd2b7cebf3cf581
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-04 13:39:52 -0500

    project initialization

---
+++ src/main/java/com/CSC207/connecthub/App.java
@@ -0,0 +1,13 @@
+package com.CSC207.connecthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CSC207/connecthub/ConnectHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC207.connecthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
