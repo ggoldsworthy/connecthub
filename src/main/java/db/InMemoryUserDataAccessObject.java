a8aa09b21fd069dd7ad6212af4f04239838bc8cf
izabellemarianne <izabellemarianne08@gmail.com>
2024-11-10 22:14:48 -0500

    started on sign up use case

---
+++ src/main/java/db/InMemoryUserDataAccessObject.java
@@ -0,0 +1,39 @@
+package db;
+
+import entity.User;
+import use_case.signup.SignupDataAccessInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * In-memory implementation of the DAO for storing user data. This implementation does
+ * NOT persist data between runs of the program.
+ */
+public class InMemoryUserDataAccessObject implements SignupDataAccessInterface {
+
+    private final Map<String, User> users = new HashMap<>();
+
+    private String currentUser;
+
+    @Override
+    public boolean existsByUsername(String identifier) {
+        return users.containsKey(identifier);
+    }
+
+    @Override
+    public boolean existsByID(String userID) {
+        return false;
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public void save(User user) {
+        users.put(user.getName(), user);
+    }
+
+}
--- src/main/java/use_case/signup/SignupDataAccessInterface.java
+++ src/main/java/use_case/signup/SignupDataAccessInterface.java
@@ -5,14 +5,28 @@ import entity.User;
 /**
  * DAO for the Signup Use Case.
  */
-public interface SignupUserDataAccessInterface {
+public interface SignupDataAccessInterface {
 
     /**
      * Checks if the given username exists.
      * @param username the username to look for
      * @return true if a user with the given username exists; false otherwise
      */
-    boolean existsByName(String username);
+    boolean existsByUsername(String username);
+
+    /**
+     * Checks if the given userID exists.
+     * @param userID the username to look for
+     * @return true if a user with the given userID exists; false otherwise
+     */
+    boolean existsByID(String userID);
+
+    /**
+     * Checks if the given email exists.
+     * @param email the email to look for
+     * @return true if a user with the given email exists; false otherwise
+     */
+    boolean existsByEmail(String email);
 
     /**
      * Saves the user.
