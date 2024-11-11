eca20125ccc179a7a4e67d6e86132a002b5a9f14
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-11 15:24:15 -0500

    added entities PostContent and CommentContent

---
+++ src/main/java/entity/CommentContent.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class CommentContent extends Content {
+    public CommentContent(String body, String attachmentPath, String fileType) {
+        super(body, attachmentPath, fileType);
+    }
+}
---
+++ src/main/java/entity/PostContent.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class PostContent extends Content {
+    public PostContent(String body, String attachmentPath, String fileType) {
+        super(body, attachmentPath, fileType);
+    }
+}
