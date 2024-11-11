141f0e4504330739df9113d577961152b71bb4db
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-11 15:43:09 -0500

    added entity CommentFactory

---
+++ src/main/java/entity/CommentFactory.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentFactory {
+    public Comment createComment(User author, String content, String attachmentPath, String fileType) {
+        Content commentContent = new CommentContent(content, attachmentPath, fileType);
+        String entryID = generateID();
+        List<Comment> replies = new ArrayList<Comment>();
+        Comment comment = new Comment(entryID, author, commentContent, LocalDateTime.now(), null, 
+                             0, 0, replies);
+        return comment;
+    }
+
+    private String generateID() {
+        // TODO same as PostFactory, maybe they could share a util function
+        return "";
+    }
+}
