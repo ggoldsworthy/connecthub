75d8112f2074e42fafc515e2bdf6d72ebf2b229f
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-11 15:16:13 -0500

    created entity Content

---
+++ src/main/java/entity/Content.java
@@ -0,0 +1,37 @@
+package entity;
+
+public class Content {
+    private String body;
+    private String attachmentPath;
+    private String fileType;
+
+    public Content(String body, String attachmentPath, String fileType) {
+        this.body = body;
+        this.attachmentPath = attachmentPath;
+        this.fileType = fileType;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAttachmentPath() {
+        return attachmentPath;
+    }
+
+    public void setAttachmentPath(String attachmentPath) {
+        this.attachmentPath = attachmentPath;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+}
