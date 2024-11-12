6a74907ef5c55de858b1009ba596fc4721fd3ae3
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-11 21:11:29 -0500

    more docs for factories

--- src/main/java/entity/CommentFactory.java
+++ src/main/java/entity/CommentFactory.java
@@ -4,7 +4,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Responsible for creating a comment.
+ */
 public class CommentFactory {
+    /**
+     * Creates a comment.
+     * See Comment and ForumEntry for the meaning of the parameters.
+     * @return the Comment object created.
+     */
     public Comment createComment(User author, String content, String attachmentPath, String fileType) {
         Content commentContent = new CommentContent(content, attachmentPath, fileType);
         String entryID = generateID();
--- src/main/java/entity/PostFactory.java
+++ src/main/java/entity/PostFactory.java
@@ -4,7 +4,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Responsible for creating a post.
+ */
 public class PostFactory {
+    /**
+     * Creates a post.
+     * See Post and ForumEntry for the meaning of the parameters.
+     * @return the Post object created.
+     */
     public Post createPost(User author, String content, String attachmentPath, String fileType, 
                            String postTitle, String category) {
         Content postContent = new PostContent(content, attachmentPath, fileType);
