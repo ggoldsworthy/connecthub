da3dddc0264c6ddedb31b8023bed36087d4a255e
Karththigan <kpush040@uottawa.ca>
2024-11-14 00:12:27 -0500

    Fix methods in GetPostDAO

--- src/main/java/db/DBPostDataAccessObject.java
+++ src/main/java/db/DBPostDataAccessObject.java
@@ -1,44 +1,51 @@
 package db;
 
-import entity.Post;
-import use_case.post.PostDataAccessInterface;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import entity.Post;
+import use_case.getpost.GetPostDataAccessInterface;
 
 /**
  * In-memory implementation of the DAO for storing post data.
  * This implementation does NOT persist data between runs of the program.
  */
-public class DBPostDataAccessObject implements PostDataAccessInterface {
+public class DBPostDataAccessObject implements GetPostDataAccessInterface {
 
     private final Map<String, Post> posts = new HashMap<>();
 
     @Override
-    public Post getPostById(String postId) {
+    public Post getPostByEntryID(String postId) {
         return posts.get(postId);
     }
 
     @Override
-    public List<Post> getAllPostsByUser(String userId) {
-        List<Post> userPosts = new ArrayList<>();
+    public List<Post> getAllPostsByUserID(String userId) {
+        final List<Post> userPosts = new ArrayList<>();
         for (Post post : posts.values()) {
-            if (post.getUserId().equals(userId)) {
+            if (post.getAuthor().getName().equals(userId)) {
                 userPosts.add(post);
             }
         }
         return userPosts;
     }
 
-    @Override
+    /**
+     * Save a post.
+     * @param post the Post to save
+     */
     public void savePost(Post post) {
-        posts.put(post.getId(), post);
+        posts.put(post.getEntryID(), post);
     }
 
-    @Override
-    public boolean deletePost(String postId) {
-        return posts.remove(postId) != null;
+    /**
+     * Delete a post.
+     * @param entryID the entryID of the Post to delete
+     * @return true if the post with entryID was deleted; false if such a post does not exist
+     */
+    public boolean deletePost(String entryID) {
+        return posts.remove(entryID) != null;
     }
 }
