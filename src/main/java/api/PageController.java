4f7f086f5f9930747722214e6db1b9a8b7c9f94c
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-18 15:03:39 -0500

    started web server

---
+++ src/main/java/api/PageController.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "index";
+    }
+}
