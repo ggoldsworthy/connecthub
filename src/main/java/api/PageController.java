69b8eab1e793ee84b646a156937ab0f3c80b2fbe
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-18 15:19:56 -0500

    templated view model for home page, post, login and signup

--- src/main/java/api/PageController.java
+++ src/main/java/api/PageController.java
@@ -2,11 +2,29 @@ package api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
+    private final String TEMPLATE_FILE = "index";
+
     @GetMapping("/")
-    public String helloWorld() {
-        return "index";
+    public String homePage() {
+        return TEMPLATE_FILE;
+    }
+
+    @GetMapping("/signin")
+    public String signIn() {
+        return TEMPLATE_FILE;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return TEMPLATE_FILE;
+    }
+
+    @GetMapping("/post/{id}")
+    public String post(@PathVariable("id") String postID) {
+        return TEMPLATE_FILE;
     }
 }
