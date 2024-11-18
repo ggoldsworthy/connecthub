8163161141b077fbc84c54e5b688f5f94c329939
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-18 15:43:51 -0500

    templated bundle loading and page title

--- src/main/java/app/WebServer.java
+++ src/main/java/app/WebServer.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"app", "api"})
-public class ConnectHubWebServer {
+public class WebServer {
     public static void main(String[] args) {
         final Repositories repositories = new Repositories();
 
-        SpringApplication.run(ConnectHubWebServer.class, args);
+        SpringApplication.run(WebServer.class, args);
         
         Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
