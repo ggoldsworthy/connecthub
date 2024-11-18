b532a1d0f4da062a3900ae5998e2d08694381540
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-18 15:07:40 -0500

    rename web server file name

--- src/main/java/app/ConnectHubWebServer.java
+++ src/main/java/app/ConnectHubWebServer.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"app", "api"})
-public class App {
+public class ConnectHubWebServer {
     public static void main(String[] args) {
         final Repositories repositories = new Repositories();
 
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(ConnectHubWebServer.class, args);
         
         Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
