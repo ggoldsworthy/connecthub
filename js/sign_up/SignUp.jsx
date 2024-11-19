761e1a35c0b1779afb2e3e6ad34d7500c47d0d44
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-19 13:00:35 -0500

    removed useState hook and added more ref hooks instead

--- js/sign_up/SignUp.jsx
+++ js/sign_up/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useRef } from "react"
 import Entry from "../common/Entry.jsx"
 
 export default function SignUp() {
@@ -6,28 +6,16 @@ export default function SignUp() {
 }
 
 function SignUpForm() {
-  const [data, setData] = useState({
-    email: "", 
-    userName: "",
-    fullName: "",
-    password: "",
-    confirmPassword: "",
-  })
-
+  const emailRef = useRef(null)
+  const usernameRef = useRef(null)
+  const fullNameRef = useRef(null)
   const passwordRef = useRef(null)
   const confirmPasswordRef = useRef(null)
   const matchMessage = useRef(null)
 
-  const handlePasswordChange = (e) => {
+  const handlePasswordChange = () => {
     const passwordVal = passwordRef.current.value;
     const confirmPasswordVal = confirmPasswordRef.current.value;
-
-    setData((prevData) => ({
-        ...prevData,
-        password: passwordVal,
-        confirmPassword: confirmPasswordVal,
-    }))
-
     matchMessage.current.innerText = passwordVal === confirmPasswordVal ?  "" : "password don't match";
   }
   
@@ -35,17 +23,17 @@ function SignUpForm() {
     <form className="form" action="">
         <div className="input-group">
         <label>Email: </label>
-        <input name="email" id="email-input" placeholder="doctor.giggle.touch@gmail.com" />
+        <input name="email" id="email-input" placeholder="doctor.giggle.touch@gmail.com" ref={emailRef} />
       </div>
 
       <div className="input-group">
         <label>Username: </label>
-        <input name="username" id="username-input" placeholder="ProfessorHankySpanky" />
+        <input name="username" id="username-input" placeholder="ProfessorHankySpanky" ref={usernameRef} />
       </div>
       
       <div className="input-group">
         <label>Full Name: </label>
-        <input name="fullName" id="full-name-input" placeholder="Doodle Wacker" />
+        <input name="fullName" id="full-name-input" placeholder="Doodle Wacker" ref={fullNameRef} />
       </div>
 
       <div className="input-group">
