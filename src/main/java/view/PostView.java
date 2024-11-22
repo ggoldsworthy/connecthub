59758fa048ebaee67ab7e28fc089193d59d6176a
Karththigan <kpush040@uottawa.ca>
2024-11-21 22:19:39 -0500

    Add Back Button to Post View

--- src/main/java/view/PostView.java
+++ src/main/java/view/PostView.java
@@ -21,16 +21,37 @@ public class PostView extends JPanel {
         postView.setBackground(new Color(120, 133, 133));
 
         // Post title
-        JLabel postTitle = new JLabel(title);
+        final JPanel titlePanel = new JPanel(new BorderLayout());
+        final JLabel postTitle = new JLabel(title);
         postTitle.setFont(new Font(FONT_TYPE, Font.BOLD, 16));
         postTitle.setForeground(Color.DARK_GRAY);
         postTitle.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1),
                 BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-        postView.add(postTitle, BorderLayout.NORTH);
+
+        // Add the title to the left side of the title panel
+        titlePanel.add(postTitle, BorderLayout.WEST);
+
+        final JButton backButton = new JButton("Back");
+        backButton.setFont(new Font(FONT_TYPE, Font.BOLD, 14));
+        backButton.setBackground(new Color(200, 200, 200));
+        backButton.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+
+        // Back button action: Go back to the home page
+        backButton.addActionListener(e -> {
+            // Return to home
+            final CardLayout cl = (CardLayout) mainContent.getLayout();
+            cl.show(mainContent, "Homepage");
+        });
+
+        // Add the back button to the right side of the title panel
+        titlePanel.add(backButton, BorderLayout.EAST);
+
+        // Add title panel to main p
+        postView.add(titlePanel, BorderLayout.NORTH);
 
         // Post content
-        JTextArea postContent = new JTextArea(content);
+        final JTextArea postContent = new JTextArea(content);
         postContent.setFont(new Font(FONT_TYPE, Font.PLAIN, 14));
         postContent.setForeground(Color.BLACK);
         postContent.setLineWrap(true);
@@ -43,7 +64,7 @@ public class PostView extends JPanel {
         postView.add(new JScrollPane(postContent), BorderLayout.CENTER);
 
         // Comments section
-        JPanel commentsPanel = new JPanel();
+        final JPanel commentsPanel = new JPanel();
         commentsPanel.setLayout(new BoxLayout(commentsPanel, BoxLayout.Y_AXIS));
         commentsPanel.setBackground(Color.WHITE);
         commentsPanel.setBorder(BorderFactory.createTitledBorder(
@@ -55,7 +76,7 @@ public class PostView extends JPanel {
                 Color.DARK_GRAY));
 
         addComments(commentsPanel);
-        JScrollPane commentsScrollPane = new JScrollPane(commentsPanel);
+        final JScrollPane commentsScrollPane = new JScrollPane(commentsPanel);
         commentsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         postView.add(commentsScrollPane, BorderLayout.SOUTH);
 
@@ -64,7 +85,7 @@ public class PostView extends JPanel {
 
     private void addComments(JPanel commentsPanel) {
         for (int i = 1; i <= 5; i++) {
-            JLabel comment = new JLabel("Comment " + i + ": This is a sample comment.");
+            final JLabel comment = new JLabel("Comment " + i + ": This is a sample comment.");
             comment.setFont(new Font(FONT_TYPE, Font.PLAIN, 12));
             comment.setForeground(Color.DARK_GRAY);
             comment.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
