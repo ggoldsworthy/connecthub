e7ccda1ca01bf3309922bae654a434eb18d3c879
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-23 15:32:19 -0500

    post controller

--- src/main/java/controller/post/PostController.java
+++ src/main/java/controller/post/PostController.java
@@ -1,6 +1,5 @@
 package controller.post;
 
-import entity.Post;
 import use_case.getpost.GetPostInputBoundary;
 import use_case.getpost.GetPostInputData;
 
@@ -18,8 +17,6 @@ public class PostController {
         final GetPostInputData getPostInputData = new GetPostInputData(postId);
         try {
             this.getPostInteractor.getPost(getPostInputData);
-        } catch (IllegalArgumentException ex) {
-
         } catch (Exception ex) {
 
         }
