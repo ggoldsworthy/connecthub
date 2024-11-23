073404e032b985ca73b416a33fc2fcfc635d533e
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-23 15:29:49 -0500

    rest of the controller

--- src/main/java/controller/post/PostController.java
+++ src/main/java/controller/post/PostController.java
@@ -1,5 +1,27 @@
 package controller.post;
 
+import entity.Post;
+import use_case.getpost.GetPostInputBoundary;
+import use_case.getpost.GetPostInputData;
+
+/**
+ * A controller for an individual post
+ */
 public class PostController {
-    
+    private final GetPostInputBoundary getPostInteractor; 
+
+    public PostController(GetPostInputBoundary getPostInteractor) {
+        this.getPostInteractor = getPostInteractor;
+    }
+
+    public void execute(String postId) {
+        final GetPostInputData getPostInputData = new GetPostInputData(postId);
+        try {
+            this.getPostInteractor.getPost(getPostInputData);
+        } catch (IllegalArgumentException ex) {
+
+        } catch (Exception ex) {
+
+        }
+    }
 }
--- src/main/java/controller/post/PostState.java
+++ src/main/java/controller/post/PostState.java
@@ -6,31 +6,38 @@ import entity.Comment;
 
 public class PostState {
     private String postContent = "";
-    private String fullNameError;
+    private String postContentError;
     private List<Comment> comments = new ArrayList<>();
     private List<Comment> commentsError;
     
     public String getPostContent() {
         return postContent;
     }
+
     public void setPostContent(String postContent) {
         this.postContent = postContent;
     }
-    public String getFullNameError() {
-        return fullNameError;
+
+    public String getPostContentError() {
+        return postContentError;
     }
-    public void setFullNameError(String fullNameError) {
-        this.fullNameError = fullNameError;
+
+    public void setPostContentError(String postContentError) {
+        this.postContentError = postContentError;
     }
+
     public List<Comment> getComments() {
         return comments;
     }
+
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
+
     public List<Comment> getCommentsError() {
         return commentsError;
     }
+
     public void setCommentsError(List<Comment> commentsError) {
         this.commentsError = commentsError;
     }
