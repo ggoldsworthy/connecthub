39d760ac09c4dae3b149457c5a78d85ef43d544e
lucas <lucasliu.liu@mail.utoronto.ca>
2024-11-23 15:47:27 -0500

    - log in state is complete

--- src/main/java/controller/login/LoginState.java
+++ src/main/java/controller/login/LoginState.java
@@ -1,7 +1,35 @@
 package controller.login;
 
+/**
+ * The state for the Login View Model.
+ */
 public class LoginState {
-    public LoginState() {
-        
+    private String email = "";
+    private String loginError;
+    private String password = "";
+
+    public String getEmail() {
+        return email;
     }
+
+    public String getLoginError() {
+        return loginError;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLoginError(String emailError) {
+        this.loginError = emailError;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
