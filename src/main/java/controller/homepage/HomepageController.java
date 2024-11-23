69faa2b877792974c2c34e3d5141125e5d108907
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-23 01:15:23 -0500

    load post contents on to home page

--- src/main/java/controller/homepage/HomepageController.java
+++ src/main/java/controller/homepage/HomepageController.java
@@ -1,4 +1,21 @@
 package controller.homepage;
 
+import java.util.List;
+
+import org.json.JSONArray;
+
+import entity.Post;
+import use_case.getpost.GetPostInputBoundary;
+
 public class HomepageController {
+    private final GetPostInputBoundary getPostInteractor;
+
+    public HomepageController(GetPostInputBoundary getPostInteractor) {
+        this.getPostInteractor = getPostInteractor;
+    }
+
+    public List<Post> fetchPosts(int size) {
+        List<Post> posts = this.getPostInteractor.fetchPosts(); // TODO edit
+        return posts.subList(size);
+    }
 }
