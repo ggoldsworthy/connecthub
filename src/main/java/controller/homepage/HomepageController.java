9043c42efb4f4349e4a3b6bb3be040853f5d2841
Karththigan <kpush040@uottawa.ca>
2024-11-23 00:07:34 -0500

    Temp commit of files

--- src/main/java/controller/homepage/HomepageController.java
+++ src/main/java/controller/homepage/HomepageController.java
@@ -1,4 +1,14 @@
-package controller.homepage;
-
-public class HomepageController {
-}
+//package controller.homepage;
+//
+//public class HomepageController {
+//    public Controller(GetPostInputBoundary getPostInteractor) {
+//    }
+//} but wait so like the actual construction of the homepage is still going to stay in connecthub.java right??
+//
+//yea we are constructing the home page, but it has no posts filled in it,
+//thats what the states are for: the
+//
+//like u can u can use the get post interac
+//stnetnoc eht ni llif dluoc ew os ata
+//
+//oh hmm so the populating of the posts should bbeoh hmm so the populating of the posts should bbe done here (like we'd need a hoepage  interactor?)
