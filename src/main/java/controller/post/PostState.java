5b3f2f720c5da910d3958953735d6de23dc51a5c
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-23 14:49:25 -0500

    add post state

--- src/main/java/controller/post/PostState.java
+++ src/main/java/controller/post/PostState.java
@@ -1,5 +1,44 @@
 package controller.post;
 
+import java.util.ArrayList;
+import java.util.List;
+import entity.Comment;
+
 public class PostState {
+    private String postContent = "";
+    private String fullNameError;
+    private List<Comment> comments = new ArrayList<>();
+    private List<Comment> commentsError;
     
+    public String getPostContent() {
+        return postContent;
+    }
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+    public String getFullNameError() {
+        return fullNameError;
+    }
+    public void setFullNameError(String fullNameError) {
+        this.fullNameError = fullNameError;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    public List<Comment> getCommentsError() {
+        return commentsError;
+    }
+    public void setCommentsError(List<Comment> commentsError) {
+        this.commentsError = commentsError;
+    }
+
+    @Override
+    public String toString() {
+        return "PostState{" 
+            + "post_content='" + postContent + '\''
+            + "}";
+    }
 }
