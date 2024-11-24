6506a72024359808b79ab118eb9b868278788b4a
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-24 14:49:56 -0500

    basic nav bar

--- js/common/NavBar.jsx
+++ js/common/NavBar.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { navigateTo } from "../api/utils.js"
 import logo from "../assets/logo.png"
 
 export default function NavBar(props) {
   const [username, setUsername] = useState("")
+  const [handle, setHandle] = useState("") // = `@${username.toLowerCase().replace(" ", "_")}`
+
+  useEffect(() => {
+    // fetch for current user
+  }, [])
 
   return (
-    <div id="nav-bar-container">
+    <div id="nav-bar-container" onClick={() => navigateTo("/")}>
       <div id="nav-bar-home-container" >
         <img src={logo} alt="home logo" />
         <div>Connect Hub</div>
       </div>
 
       <div id="nav-bar-user-container">
-        <div>{username}</div>
-        <img src="" alt="user profile pic" />
+        <img src="" alt=" " />
+        <div className="username">{"Need Use Case"}</div>
+        <div className="handle">{"@need_use_case"}</div>
       </div>
     </div>
   )
