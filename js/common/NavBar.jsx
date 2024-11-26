020705fe80464f028649c48586c6650280384ffe
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-26 15:39:05 -0500

    add current user to nav bar

--- js/common/NavBar.jsx
+++ js/common/NavBar.jsx
@@ -1,20 +1,40 @@
 import { useEffect, useState, useRef } from "react"
 import { navigateTo } from "../api/utils.js"
+import api from "../api/axios.config.js"
 import logo from "../assets/logo.png"
+
+const GET_CURRENT_USER_URL = "/current-user"
 
 export default function NavBar(props) {
   const [username, setUsername] = useState("")
-  const [handle, setHandle] = useState("") // = `@${username.toLowerCase().replace(" ", "_")}`
+  const [handle, setHandle] = useState("")
 
   const searchBarRef = useRef(null)
 
+  const formatHandle = (username => {
+    return `@${username.toLowerCase().replace(" ", "_")}`
+  })
+
   useEffect(() => {
     // fetch for current user
+    api
+      .get(GET_CURRENT_USER_URL)
+      .then(response => {
+        if (response.data === "") {
+          setUsername("")
+          setHandle("")
+        } else {
+          setUsername(response.data.username)
+          setHandle(formatHandle(response.data.username))
+        }
+        console.log(response)
+      })
+      .catch(() => {
+      })
 
     // binding key events for search bar
     const searchBar = searchBarRef.current
 
-    // Add event listener
     if (searchBar) { searchBar.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
         e.preventDefault();
@@ -44,8 +64,8 @@ export default function NavBar(props) {
 
       <div id="nav-bar-user-container">
         <img src="" alt=" " />
-        <div className="username">{"Need Use Case"}</div>
-        <div className="handle">{"@need_use_case"}</div>
+        <div className="username">{username}</div>
+        <div className="handle">{handle}</div>
       </div>
     </div>
   )
